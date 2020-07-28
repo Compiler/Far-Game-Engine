63693533a1c313eab706736ae27114488305ec56
Compiler <lukerochedev@gmail.com>
2020-07-28 15:07:23 -0400

    fixed make

--- FarEngine/src/Files/TextLoader.h
+++ FarEngine/src/Files/TextLoader.h
@@ -6,11 +6,6 @@
 //#pragma warning (disable : 4996)
 namespace far {
 
-	void loadTextFromFile(const char fileName[], std::string& dst){
-
-		std::ifstream t(fileName);
-        std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
-        dst = str;
-    }
+	void loadTextFromFile(const char fileName[], std::string& dst);
 
 }
