a386b0846adfd3bc4920580b5c2ae9fa0f4a7f32
compiler <lukerochedev@gmail.com>
2020-07-30 17:38:02 -0400

    logging system done

--- FarEngine/src/Resources/Shaders/pass.frag
+++ FarEngine/src/Resources/Shaders/pass.frag
@@ -6,7 +6,7 @@ layout(location = 1) in vec2 o_texCoords;
 
 layout(location = 0) out vec4 outColor;
 
-uniform sampler2D u_texture;
+//uniform sampler2D u_texture;
 
 void main(){
 // /texture(u_texture, o_texCoords) + 
--- FarEngine/src/main.cpp
+++ FarEngine/src/main.cpp
@@ -1,17 +1,22 @@
-#include <FarCore.h>
+#include <Tools/Logging/TmpLog.h>
+//#include <FarCore.h>
 
 int main(int argc, char* argv[]){
 
-
-	far::FarCore* core = new far::FarCore();
-	
-	
-	core->load();
-	while(core->isRunning()){
-		core->update();
-		core->render();
-	}
-	core->unload();
+	FAR_LOG("Sup dude THIS IS A LOG");
+	FAR_DEBUG("Sup dude THIS IS A DEBUG");
+	FAR_WARN("Sup dude THIS IS A WARNING");
+	FAR_ERROR("Sup dude THIS IS AN ERROR");
+	//return 1;
+	//far::FarCore* core = new far::FarCore();
+	//
+	//
+	//core->load();
+	//while(core->isRunning()){
+	//	core->update();
+	//	core->render();
+	//}
+	//core->unload();
 	
 }
 
