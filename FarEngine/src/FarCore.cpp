65eb13559422a2b52c10132f0886f3e3d7e41d67
compiler <lukerochedev@gmail.com>
2020-07-30 15:52:10 -0400

    fixed updating glBuffers and 'make launch'

--- FarEngine/src/FarCore.cpp
+++ FarEngine/src/FarCore.cpp
@@ -37,7 +37,8 @@ namespace far{
     }
 
     void FarCore::render(){
-        glClearColor(0.2, 0.1, 0.2, 1.0);
+		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+        glClearColor(0.9, 0.4, 0.2, 1.0);
 		glUseProgram(shaderProgram);
 		int loc = glGetUniformLocation(shaderProgram, "time");
 		glUniform1f(loc, glfwGetTime());
--- FarEngine/src/main.cpp
+++ FarEngine/src/main.cpp
@@ -1,22 +1,17 @@
-//#include <FarCore.h>
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/basic_file_sink.h>
+#include <FarCore.h>
 
 int main(int argc, char* argv[]){
 
 
-	spdlog::info("Test 1");
-
-	return 3;
-	//far::FarCore* core = new far::FarCore();
-//
-//
-	//core->load();
-	//while(core->isRunning()){
-	//	core->update();
-	//	core->render();
-	//}
-	//core->unload();
+	far::FarCore* core = new far::FarCore();
+	
+	
+	core->load();
+	while(core->isRunning()){
+		core->update();
+		core->render();
+	}
+	core->unload();
 	
 }
 
